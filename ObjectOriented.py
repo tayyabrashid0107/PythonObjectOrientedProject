80570b5832b8a07ad506151edf86d4fc79d54a98
tayyabrashid0107 <tayyab.m272@gmail.com>
2017-01-15 00:39:03 +0100

    The project file

---
+++ ObjectOriented.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 14 02:23:20 2017
+
+@author: Tayyab Rashid
+
+We will be builing an application which will have some function for school. 
+This will a simple object oriented class. Class will be Student. We will give 
+instroction on a sincle student, and thereafter we will have som variable 
+student and total list of all the student.
+"""
+
+class Student:
+    StudentList=[]
+    Num_of_Students=0
+    def __init__(self, first, second, Origin):
+        self.first=first #First name
+        self.second=second #Second name or surname
+        self.Origin=Origin #Domestic or foreig student
+        self.email=first+"."+second+"@Highschool.com"#Email adress is created 
+        Student.Num_of_Students+=1
+        Student.StudentList.append("{} {}".format(self.first, self.second)) 
+    
+    @property #This is a getter we get fullname as an attribute
+    def fullname(self): #Regular methods take instane as the first argument
+        return "{} {}".format(self.first, self.second)
+
+    @fullname.setter #This we use to pass whole name to change first and second
+    def fullname(self,name):
+        first, second=name.split(' ')
+        self.first=first
+        self.second=second
+        
+    @fullname.deleter #This one we use to delete a name del dev_1.fullname
+    def fullname(self):
+        print("Delete name!")
+        self.first=None
+        self.second=None
+
+class Teacher(Student):#inheritated from Student
+    def __init__(self, first, second, Origin, Students=None):
+        super().__init__(first, second, Origin)
+        if Students is None:
+            self.Students=[]
+        else:
+            self.Students=Students
+    def add_std(self, std):
+        if std not in self.Students:
+            self.Students.append(std)
+    
+    def remove_std(self, std):
+        if std not in self.Students:
+            self.Students.remove(std)
+    def print_std(self):
+        for std in self.Students:
+            print("-->", std.fullname)
+        
+        
+        
